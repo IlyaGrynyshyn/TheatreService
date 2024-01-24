118f198265a855c3ec5007afc22fd958cf2eb389
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-24 19:26:05 +0200

    Implement view and url for genres

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import PlayViewSet, TheatreHallViewSet, PerformanceViewSet, ActorViewSet
+from theatre.views import (
+    PlayViewSet,
+    TheatreHallViewSet,
+    PerformanceViewSet,
+    ActorViewSet,
+    GenreViewSet
+)
 
 router = routers.DefaultRouter()
 router.register("actors", ActorViewSet, basename="actors")
+router.register("genres", GenreViewSet, basename="genres")
 router.register("plays", PlayViewSet, basename="plays")
 router.register("theatre_halls", TheatreHallViewSet, basename="theatre_halls")
 router.register("performance", PerformanceViewSet, basename="performance")
--- theatre/views.py
+++ theatre/views.py
@@ -1,17 +1,29 @@
 from rest_framework import viewsets
 
-from theatre.models import Play, TheatreHall, Performance, Actor
+from theatre.models import (
+    Play,
+    TheatreHall,
+    Performance,
+    Actor,
+    Genre
+)
 from theatre.serializers import (
     PlaySerializer,
     TheatreHallSerializer,
     PerformanceSerializer,
-    ActorSerializer
+    ActorSerializer,
+    GenreSerializer
 )
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     serializer_class = ActorSerializer
     queryset = Actor.objects.all()
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
 
 
 class PlayViewSet(viewsets.ModelViewSet):
