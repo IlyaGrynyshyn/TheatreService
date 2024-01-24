8e118b8c439e982a3e53d8bbe0a639da1223ab84
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-24 18:32:12 +0200

    Implement view & urls for play, theatre hall and performance

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import PlayViewSet, TheatreHallViewSet, PerformanceViewSet
+
+router = routers.DefaultRouter()
+router.register("plays", PlayViewSet, basename="plays")
+router.register("theatre_halls", TheatreHallViewSet, basename="theatre_halls")
+router.register("performance", PerformanceViewSet, basename="performance")
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import Play, TheatreHall, Performance
+from theatre.serializers import PlaySerializer, TheatreHallSerializer, PerformanceSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    serializer_class = PlaySerializer
+    queryset = Play.objects.all()
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    serializer_class = TheatreHallSerializer
+    queryset = TheatreHall.objects.all()
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    serializer_class = PerformanceSerializer
+    queryset = Performance.objects.all()
