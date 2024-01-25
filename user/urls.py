c41eeb19d84ea9f8a22e005db61517c68db78e22
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-25 17:34:45 +0200

    Implement endpoint profile

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
-    path("token/", CreateTokenView.as_view(), name="token")
+    path("token/", CreateTokenView.as_view(), name="token"),
+    path("profile/", ManageUserView.as_view(), name="profile")
 ]
