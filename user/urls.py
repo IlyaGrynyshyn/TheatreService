93a55a13d13020ebcb2ac50199c720d800b094c7
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-25 17:23:47 +0200

    Implement endpoint create token

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", CreateTokenView.as_view(), name="token")
 ]
