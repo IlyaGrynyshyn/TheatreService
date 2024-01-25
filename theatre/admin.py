8f919d1b477a885a52e8b2c4292e1aded1062941
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-25 23:14:13 +0200

    Implement admin.py for theatre app

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import (
+    TheatreHall,
+    Play,
+    Performance,
+    Actor,
+    Genre,
+    Ticket,
+    Reservation
+)
+
+admin.site.register(TheatreHall)
+admin.site.register(Play),
+admin.site.register(Performance),
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Ticket)
+admin.site.register(Reservation)
