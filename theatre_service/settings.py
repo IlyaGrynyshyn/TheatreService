0a7a821cd924d8075cd9cd2b3fd83caa63e28443
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-25 18:30:30 +0200

    Add SPECTACULAR_SETTINGS in settings.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -131,3 +131,9 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Theatre Service",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
