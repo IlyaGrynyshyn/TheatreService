8e15cdc44ce6a71e0dffc4e724125667bc69f542
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-25 18:27:20 +0200

    Implement swagger

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "drf_spectacular",
     "theatre",
     "user",
 ]
@@ -126,3 +127,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
